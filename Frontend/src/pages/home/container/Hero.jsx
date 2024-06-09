29410ece1309e1e10fca42c17fc937598bf32109
verma <vermasanjay1393@gmail.com>
2024-06-09 09:36:25 -0400

    upload on github

---
+++ Frontend/src/pages/home/container/Hero.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { getAllPosts } from "../../../services/index/posts";
+import ErrorMessage from "../../../components/ErrorMessage";
+import { useSearchParams } from "react-router-dom";
+import { images } from "../../../constants";
+import Search from "../../../components/Search";
+
+let isFirstRun = true;
+
+const Hero = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const searchParamsValue = Object.fromEntries([...searchParams]);
+
+  const currentPage = parseInt(searchParamsValue?.page) || 1;
+  const searchKeyword = searchParamsValue?.search || "";
+
+  const { data, isLoading, isError, isFetching, refetch } = useQuery({
+    queryFn: () => getAllPosts(searchKeyword, currentPage, 12),
+    queryKey: ["posts"],
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
+
+  console.log(data);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    if (isFirstRun) {
+      isFirstRun = false;
+      return;
+    }
+    refetch();
+  }, [currentPage, searchKeyword, refetch]);
+
+  const handlePageChange = (page) => {
+    // change the page's query string in the URL
+    setSearchParams({ page, search: searchKeyword });
+  };
+
+  const handleSearch = ({ searchKeyword }) => {
+    setSearchParams({ page: 1, search: searchKeyword });
+  };
+
+  return (
+    <section className="container mx-auto flex flex-col px-5 py-5 lg:flex-row">
+      <div className="mt-10 lg:w-1/2">
+        <h1 className="font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-4xl xl:text-5xl lg:text-left lg:max-w-[540px]">
+          Read the most interesting articles
+        </h1>
+        <p className="text-dark-light mt-4 text-center md:text-xl lg:text-base xl:text-xl lg:text-left">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua
+        </p>
+        {/* <Search className="mt-10 lg:mt-6 xl:mt-10" /> */}
+        <Search
+          className="mt-10 lg:mt-6 xl:mt-10"
+          onSearchKeyword={handleSearch}
+        />
+        <div className="flex mt-4 flex-col lg:flex-row lg:items-start lg:flex-nowrap lg:gap-x-4 lg:mt-7">
+          <span className="text-dark-light font-semibold italic mt-2 lg:mt-4 lg:text-sm xl:text-base">
+            Popular Tags:
+          </span>
+          <ul className="flex flex-wrap gap-x-2.5 gap-y-2.5 mt-3 lg:text-sm xl:text-base">
+            <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold">
+              Design
+            </li>
+            <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold">
+              User Experience
+            </li>
+            <li className="rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold">
+              User Interfaces
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="hidden lg:block lg:1/2">
+        <img
+          className="w-full"
+          src={images.HeroImage}
+          alt="users are reading articles"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
