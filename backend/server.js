26e8da80d68af99c04f5d34a7dd01ec0c78c4563
Sanjay Kumar <139473767+sanjayverma139@users.noreply.github.com>
2024-08-21 12:17:29 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import connectDB from "./config/db";
+import connectDB from "./config/db.js";
 import cors from "cors";
 import {
   errorResponserHandler,
